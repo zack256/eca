d49ede2c89eee5b94f321c0d888778e15ee4bb18
zack256 <zack256@users.noreply.github.com>
2021-06-29 23:05:21 -0400

    simple tests

--- main.cpp
+++ main.cpp
@@ -1,30 +1,47 @@
 #include <iostream>
-//#include <filesystem>
+#include <vector>
 
 #include <opencv2/opencv.hpp>
-/**
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/video/video.hpp>
-#include <opencv2/imgcodecs/imgcodecs.hpp>
-**/
 
 using namespace std;
 using namespace cv;
 
+Mat create_empty_image (int rows, int cols) {
+    Scalar white = Scalar(255, 255, 255);
+    Mat m (rows, cols, CV_8UC3, white);
+    return m;
+}
+
+void draw_vector_as_row(Mat &m, vector<int> &v, int row) {
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i]) {
+            Vec3b & color = m.at<Vec3b>(row, i);
+            color[0] = 0;
+            color[1] = 0;
+            color[2] = 0;
+        }
+    }
+}
+
 int main () {
 
-    string path = "path/to/file.png";
+    int rows = 500, cols = 256;
+    Mat m = create_empty_image(rows, cols);
 
-    Mat img = imread(path);
-    if (img.empty()) {
-        cout << "File not found :("; return 0;
+    vector<int> v (cols, 0);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (j && (i % j)) {
+                v[j] = 0;
+            } else {
+                v[j] = 1;
+            }
+        }
+        draw_vector_as_row(m, v, i);
     }
-    imshow("Viewing image", img);
+
+    imshow("Viewing image", m);
     int k = waitKey(0);
     return 0;
+
 }
