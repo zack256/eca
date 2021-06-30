d3768a9ff4ea56adbf81db4c16f8edf1eb5c6dc5
zack256 <zack256@users.noreply.github.com>
2021-06-30 17:19:03 -0400

    basic eca working

--- main.cpp
+++ main.cpp
@@ -12,6 +12,26 @@ Mat create_empty_image (int rows, int cols) {
     return m;
 }
 
+int get_state_of_pos (vector<int> &v, int idx) {
+    if ((idx < 0) || (idx >= v.size())) {
+        return 0;   // here, off screen returns 0 (white)
+    }
+    return v[idx];
+}
+
+int get_new_state(vector<int> &v, vector<int> &directions, int idx) {
+    int direction = 4 * get_state_of_pos(v, idx - 1) + 2 * get_state_of_pos(v, idx) + get_state_of_pos(v, idx + 1);
+    return directions[direction];
+}
+
+vector<int> update_row(vector<int> &v, vector<int> &directions) {
+    vector<int> new_v (v.size());
+    for (int i = 0; i < v.size(); i++) {
+        new_v[i] = get_new_state(v, directions, i);
+    }
+    return new_v;
+}
+
 void draw_vector_as_row(Mat &m, vector<int> &v, int row) {
     for (int i = 0; i < v.size(); i++) {
         if (v[i]) {
@@ -23,25 +43,39 @@ void draw_vector_as_row(Mat &m, vector<int> &v, int row) {
     }
 }
 
+vector<int> make_directions_from_rule (int rule) {
+    vector<int> directions (8);
+    for (int i = 0; i < 8; i++) {
+        directions[i] = rule % 2;
+        rule /= 2;
+    }
+    return directions;
+}
+
+void eca (Mat &m, vector<int> v, int rule) {
+    vector<int> directions = make_directions_from_rule(rule);
+    for (int i = 0; i < m.rows; i++) {
+        draw_vector_as_row(m, v, i);
+        v = update_row(v, directions);
+    }
+}
+
 int main () {
 
-    int rows = 500, cols = 256;
+    int rows = 200, cols = 256;
     Mat m = create_empty_image(rows, cols);
 
-    vector<int> v (cols, 0);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (j && (i % j)) {
-                v[j] = 0;
-            } else {
-                v[j] = 1;
-            }
-        }
-        draw_vector_as_row(m, v, i);
-    }
+    vector<int> start_state (cols, 0);
+    start_state[127] = 1;
+    eca(m, start_state, 110);
 
     imshow("Viewing image", m);
     int k = waitKey(0);
+
+    if (k == 's') {
+        imwrite("/path/to/save.png", m);
+    }
+
     return 0;
 
 }
